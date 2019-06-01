8dc4885e274875b103c12571e790eee5a2372e93
galaxo420 <thomas_webley@yahoo.co.uk>
2019-06-01 12:49:52 +0100

    you all know what I'm adding

---
+++ assets/untitled.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="gamejam" tilewidth="60" tileheight="60" tilecount="100" columns="10">
+ <image source="tileset.png" width="600" height="600"/>
+</tileset>
