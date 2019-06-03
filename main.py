d17bd4be7c158a989891f9e1e0252f98440cb6f7
Danny Barthaud <danny.barthaud@open.ac.uk>
2019-06-03 07:21:53 +0100

    put fullscreen mode back for final build before submission

--- main.py
+++ main.py
@@ -86,7 +86,7 @@ def __main__():
     clock = pyglet.clock.Clock()
     clock.set_fps_limit(30)
 
-    screen = pygame.display.set_mode((600, 600))
+    screen = pygame.display.set_mode((600, 600), pygame.FULLSCREEN)
     splash_screen(screen, clock)
     engine = Engine()
     running = True
